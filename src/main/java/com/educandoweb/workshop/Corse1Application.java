2f93027cd433f774b865f7f028b81fbc80296728
davinegreiiros <davi.negreiiros36@gmail.com>
2020-08-26 12:02:54 -0300

    Project created

---
+++ src/main/java/com/educandoweb/workshop/Corse1Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Corse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Corse1Application.class, args);
+	}
+
+}
