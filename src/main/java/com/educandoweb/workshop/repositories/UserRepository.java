5d39684df69fd550cd61a2d566f2876fb1349303
davinegreiiros <davi.negreiiros36@gmail.com>
2020-08-27 09:44:53 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/workshop/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/workshop/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.educandoweb.workshop.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.educandoweb.workshop.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	
